1ccd089c43c942cccee345cf1a7f826506c2e281
BilRos <billyrossowuor@gmail.com>
2024-04-07 18:32:15 +0300

    Events on events

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+
+//code to move the dodger left when the key is pressed
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+//code to move the dodger right when the key is pressed
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Arrowright") {
+      moveDodgerRight();
+    }
+  });
